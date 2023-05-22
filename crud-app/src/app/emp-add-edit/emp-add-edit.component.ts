e359979f8ab7c617016b0fe8c8a880d6ccb669f4
Marek <marekstankiewicz6@wp.pl>
2023-05-22 17:02:07 +0200

    build form

--- crud-app/src/app/emp-add-edit/emp-add-edit.component.ts
+++ crud-app/src/app/emp-add-edit/emp-add-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-emp-add-edit',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class EmpAddEditComponent {
 
+  empForm: FormGroup;
+
+  education: string[] = [
+    'Matric',
+    'Diploma',
+    'Intermediate', 
+    'Graduate',
+    'Post Graduate'
+  ];
+
+  constructor(private _fb: FormBuilder){
+    this.empForm = this._fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      dob: '',
+      gender: '',
+      education: '',
+      company: '',
+      experience: '',
+      package: ''
+
+    })
+  }
+
+  onFormSubmit() {
+    if(this.empForm.valid) {
+      console.log(this.empForm.value);
+    }
+  }
 }
